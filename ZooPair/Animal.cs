663e1a6240d91fef47baf793289ba6eb034c7a0a
user <user@WIN-0G055PKVMAU>
2026-02-24 18:15:50 +0300

    Добавлен базовый класс Animal

---
+++ ZooPair/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Animal
+{
+    private string _name;
+    private int _age;
+    public string Name => _name;
+    public int Age => _age;
+    public Animal(string name, int age)
+    {
+        _name = name;
+        _age = age;
+    }
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Животное издает звук");
+    }
+}
