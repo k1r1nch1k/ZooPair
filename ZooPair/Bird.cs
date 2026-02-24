adfd7632b5092380f5fe4beca7696ed433e88f7a
user <user@WIN-0G055PKVMAU>
2026-02-24 18:22:30 +0300

    Добавил класс Bird

---
+++ ZooPair/Bird.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Bird : Animal
+{
+    public Bird(string name, int age) : base(name, age) { }
+    public override void MakeSound()
+    {
+        Console.WriteLine($"{Name} чирикает");
+    }
+}
