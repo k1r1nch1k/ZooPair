74a48330986e4588b45beff4bc7221a9d42391fa
My Computer <My Computer@DESKTOP-GN4PPPS>
2026-02-26 15:49:14 +0300

    добавил приветсвие

--- ZooPair/Program.cs
+++ ZooPair/Program.cs
@@ -5,6 +5,8 @@
         Zoo zoo = new Zoo();
         zoo.AddAnimal(new Mammal("Лев", 5));
         zoo.AddAnimal(new Bird("Орел", 3));
+        Console.WriteLine("Добро пожаловать в зоопарк!");
         zoo.ShowAll();
+
     }
 }
